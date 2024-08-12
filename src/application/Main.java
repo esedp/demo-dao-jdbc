5c2c5c8deaead28c68b66d8d4b8b2d2bd8c0260d
José Luiz <jlnalbertii@gmail.com>
2024-08-12 17:06:03 -0300

    SellerDaoJDBC and DaoFactory

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,11 +1,13 @@
 package application;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.util.Date;
+
 
 public class Main {
     public static void main(String[] args) {
@@ -15,6 +17,8 @@ public class Main {
         Department obj = new Department(1, "Books");
 
         Seller seller = new Seller(21, "Bob", "bob@gmail.com,", LocalDate.parse("01/12/1997", dtf),3000.0, obj);
+
+        SellerDao sellerDao = DaoFactory.createSellerDao();
 
         System.out.println(seller);
 
