6b30d637b834659bb9f7ece8d9be33c9cc8b1c0e
José Luiz <jlnalbertii@gmail.com>
2024-08-12 16:53:28 -0300

    Seller entity class

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,12 +1,22 @@
 package application;
 
 import model.entities.Department;
+import model.entities.Seller;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
 
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
         Department obj = new Department(1, "Books");
-        System.out.println(obj);
+
+        Seller seller = new Seller(21, "Bob", "bob@gmail.com,", LocalDate.parse("01/12/1997", dtf),3000.0, obj);
+
+        System.out.println(seller);
 
     }
 }
